ebac549c843e4ffafeb0e7ea67fdfb4fa883e9d1
AnhKhoa <anhkhoa1612vn@gmail.com>
2023-06-14 00:24:20 +0700

    fixed

--- traceability/traceability_web/src/pages/farmer/Process/Process.jsx
+++ traceability/traceability_web/src/pages/farmer/Process/Process.jsx
@@ -27,7 +27,6 @@ import {
 } from "@mui/material";
 import { styled, useTheme } from "@mui/material/styles";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-import { Card } from "reactstrap";
 
 const headCell = [
   { id: "id", label: "No", disableSorting: true },
