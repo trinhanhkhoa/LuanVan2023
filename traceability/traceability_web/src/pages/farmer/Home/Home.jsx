964b2b62ff86a5939d38e1e1405646a76ab5f2e2
AnhKhoa <anhkhoa1612vn@gmail.com>
2023-06-18 21:22:54 +0700

    update responsive

--- traceability/traceability_web/src/pages/farmer/Home/Home.jsx
+++ traceability/traceability_web/src/pages/farmer/Home/Home.jsx
@@ -275,7 +275,7 @@ export default function Home() {
               boxShadow:
                 "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
               backgroundColor: "#D0F5BE",
-              maxWidth: "56rem",
+              maxWidth: "60rem",
               minHeight: { xs: "5rem", md: "16rem" },
               borderRadius: "10px",
               // marginRight: "10px",
